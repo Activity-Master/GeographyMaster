167d66697a552573a233b8083f6b59d8d3889447
GedMarc <ged_marc@hotmail.com>
2019-06-07 00:14:46 +0200

    IDataConcepts implementation, before IClassification lock, Updates and document styles

--- src/main/java/com/armineasy/activitymaster/geography/GeographyMasterSystem.java
+++ src/main/java/com/armineasy/activitymaster/geography/GeographyMasterSystem.java
@@ -1,8 +1,8 @@
 package com.armineasy.activitymaster.geography;
 
-import com.armineasy.activitymaster.activitymaster.db.entities.enterprise.Enterprise;
 import com.armineasy.activitymaster.activitymaster.services.IActivityMasterProgressMonitor;
 import com.armineasy.activitymaster.activitymaster.services.IActivityMasterSystem;
+import com.armineasy.activitymaster.activitymaster.services.dto.IEnterprise;
 import com.armineasy.activitymaster.activitymaster.services.system.ISystemsService;
 import com.google.inject.Singleton;
 import com.jwebmp.guicedinjection.GuiceContext;
@@ -14,7 +14,7 @@ public class GeographyMasterSystem
 {
 
 	@Override
-	public void createDefaults(Enterprise enterprise, IActivityMasterProgressMonitor progressMonitor)
+	public void createDefaults(IEnterprise<?> enterprise, IActivityMasterProgressMonitor progressMonitor)
 	{
 
 		GuiceContext.get(ISystemsService.class)
