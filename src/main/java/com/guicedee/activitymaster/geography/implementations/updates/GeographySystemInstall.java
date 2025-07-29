7c32b5c4d9ff8b1e39c388993750defccb42c275
GedMarc <ged_marc@hotmail.com>
2025-07-29 21:38:31 +0200

    migration: builds - all extension modules updated

--- src/main/java/com/guicedee/activitymaster/geography/implementations/updates/GeographySystemInstall.java
+++ src/main/java/com/guicedee/activitymaster/geography/implementations/updates/GeographySystemInstall.java
@@ -15,8 +15,6 @@ import com.guicedee.activitymaster.geography.services.enumerations.GeographyFeat
 import com.guicedee.activitymaster.geography.services.enumerations.GeographyIPIdentificationTypes;
 import lombok.extern.log4j.Log4j2;
 
-
-
 import static com.guicedee.client.IGuiceContext.*;
 import static com.guicedee.activitymaster.geography.services.enumerations.GeographyClassifications.*;
 
@@ -25,158 +23,176 @@ import static com.guicedee.activitymaster.geography.services.enumerations.Geogra
 public class GeographySystemInstall
 		implements ISystemUpdate
 {
-
 	@Override
-	public Uni<Boolean> update(IEnterprise<?,?> enterprise)
+	public Uni<Boolean> update(Mutiny.Session session, IEnterprise<?,?> enterprise)
 	{
 		log.info("Starting geography system installation");
-
-		return Uni.createFrom().item(() -> {
-			IClassificationService<?> classificationService = get(IClassificationService.class);
-			IClassificationDataConceptService<?> dataConceptService = get(IClassificationDataConceptService.class);
-
-			GeographySystem gs = get(GeographySystem.class);
-			ISystems<?,?> system = gs.getSystem(enterprise);
-			UUID token = gs.getSystemToken(enterprise);
-
-			logProgress("Geography Master", "Creating Regional Areas");
-
-			// Create base classifications
-			classificationService.create(Planet, system);
-			classificationService.create(Languages, system, Planet);
-			classificationService.create(Continent, system, Planet);
-			classificationService.create(Currency, system, Planet);
-			classificationService.create(TimeZone, system,Planet);
-
+		
+		// Get services and system information
+		IClassificationService<?> classificationService = get(IClassificationService.class);
+		IClassificationDataConceptService<?> dataConceptService = get(IClassificationDataConceptService.class);
+		GeographySystem gs = get(GeographySystem.class);
+		ISystems<?,?> system = gs.getSystem(enterprise);
+		UUID token = gs.getSystemToken(enterprise);
+		
+		logProgress("Geography Master", "Creating Regional Areas");
+		
+		// Create base classifications sequentially
+		return classificationService.create(session, Planet, system)
+			.chain(() -> classificationService.create(session, Languages, system, Planet))
+			.chain(() -> classificationService.create(session, Continent, system, Planet))
+			.chain(() -> classificationService.create(session, Currency, system, Planet))
+			.chain(() -> classificationService.create(session, TimeZone, system, Planet))
+			
 			// Create geographic hierarchy classifications
-			classificationService.create(Country, system, Continent);
-			classificationService.create(Province, system, Country);
-			classificationService.create(Location, system, Country);
-
-			classificationService.create(PostalCode, system, Province);
-			classificationService.create(PostalCodeSuburb, system, PostalCode);
-			classificationService.create(Municipalities, system, Province);
-			classificationService.create(City, system, Municipalities);
-			classificationService.create(Town, system, City);
-
-			logProgress("Geography Master", "Creating Default Geography Classifications");
-
-			// Create geography-specific classifications
-			classificationService.create(GeographyClassifications, system);
-			classificationService.create(FeatureCodes, system, GeographyClassifications);
-
-			classificationService.create(Admin1CodeASCII, system, GeographyClassifications);
-			classificationService.create(Admin2Code, system, GeographyClassifications);
-			classificationService.create(Admin3Code, system, GeographyClassifications);
-			classificationService.create(Admin4Code, system, GeographyClassifications);
-			classificationService.create(AdminCode5, system, GeographyClassifications);
-			classificationService.create(Population, system, GeographyClassifications);
-			classificationService.create(Elevation, system, GeographyClassifications);
-			classificationService.create(DEM, system, GeographyClassifications);
-			classificationService.create(Name, system, GeographyClassifications);
-			classificationService.create(NameAscii, system, GeographyClassifications);
-			classificationService.create(AlternateNames, system, GeographyClassifications);
-			classificationService.create(Latitude, system, GeographyClassifications);
-			classificationService.create(Longitude, system, GeographyClassifications);
-			classificationService.create(CountryCode, system, GeographyClassifications);
-			classificationService.create(CountryCode2, system, GeographyClassifications);
-			classificationService.create(ContinentCode, system, GeographyClassifications);
-			classificationService.create(GeoNameID, system, GeographyClassifications);
-
-			//Lookups for geography data
-			//Static features classes
-			IClassification<?, ?> classification = classificationService.create(FeatureClass, system, GeographyClassifications);
-			for (GeographyFeatureClassesClassifications value : GeographyFeatureClassesClassifications.values())
-			{
-				classificationService.create(value.toString(), value.classificationDescription(),
-						EnterpriseClassificationDataConcepts.GeographyXClassification,
-						system,
-						0,
-						classification, identityToken);
-			}
-
-			classificationService.create(GeographyAdmin1AsciiCodes, system, GeographyClassifications);
-			classificationService.create(GeographyAsciiName, system, GeographyAdmin1AsciiCodes);
-
-			//Country Data
-			logProgress("Geography Master", "Creating Geography Involved Parties");
-			//Create Identification TYpe
-			IInvolvedPartyService<?> involvedPartyService = get(IInvolvedPartyService.class);
-			IInvolvedPartyIdentificationType<?,?> idType = involvedPartyService
-					.createIdentificationType(system, GeographyIPIdentificationTypes.ISP,
-							"An Internet Service Provider",
-							token);
-
-			logProgress("Geography Master", "Creating Planets");
-			//Create Planets and Continents by default
-			IGeographyService<?> service = get(IGeographyService.class);
-			service.createPlanet("Earth", null, system, identityToken);
-
-			logProgress("Geography Master", "Creating Continents");
-			service.createContinent("Earth", new GeographyContinent().setContinentName("Africa")
-													 .setContinentCode("AF"), system, "6255146", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("Asia")
-													 .setContinentCode("AS"), system, "6255147", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("North America")
-													 .setContinentCode("NA"), system, "6255149", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("Europe")
-													 .setContinentCode("EU"), system, "6255148", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("Oceania")
-													 .setContinentCode("OC"), system, "6255151", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("South America")
-													 .setContinentCode("SA"), system, "6255150", identityToken);
-			service.createContinent("Earth", new GeographyContinent().setContinentName("Antarctica")
-													 .setContinentCode("AN"), system, "6255152", identityToken);
-
-			logProgress("Geography Master", "Creating Feature Classes");
-			//next update
-			return true;
-		})
-		.chain(() -> geonamesClassifications(enterprise).map(v -> true))
-		.onFailure().invoke(error -> log.error("Error during geography system installation: {}", error.getMessage(), error))
-		.onItem().invoke(() -> log.info("Geography system installation completed successfully"));
+			.chain(() -> classificationService.create(session, Country, system, Continent))
+			.chain(() -> classificationService.create(session, Province, system, Country))
+			.chain(() -> classificationService.create(session, Location, system, Country))
+			.chain(() -> classificationService.create(session, PostalCode, system, Province))
+			.chain(() -> classificationService.create(session, PostalCodeSuburb, system, PostalCode))
+			.chain(() -> classificationService.create(session, Municipalities, system, Province))
+			.chain(() -> classificationService.create(session, City, system, Municipalities))
+			.chain(() -> classificationService.create(session, Town, system, City))
+			
+			.chain(() -> {
+				logProgress("Geography Master", "Creating Default Geography Classifications");
+				
+				// Create geography-specific classifications
+				return classificationService.create(session, GeographyClassifications, system);
+			})
+			.chain(() -> classificationService.create(session, FeatureCodes, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Admin1CodeASCII, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Admin2Code, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Admin3Code, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Admin4Code, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, AdminCode5, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Population, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Elevation, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, DEM, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Name, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, NameAscii, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, AlternateNames, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Latitude, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, Longitude, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, CountryCode, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, CountryCode2, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, ContinentCode, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, GeoNameID, system, GeographyClassifications))
+			
+			// Create feature class
+			.chain(() -> classificationService.create(session, FeatureClass, system, GeographyClassifications))
+			.chain(classification -> {
+				// Create feature class classifications
+				Uni<IClassification<?, ?>> current = Uni.createFrom().item(classification);
+				
+				// Chain the creation of all feature class classifications
+				for (GeographyFeatureClassesClassifications value : GeographyFeatureClassesClassifications.values()) {
+					current = current.chain(c -> 
+						classificationService.create(
+							session,
+							value.toString(), 
+							value.classificationDescription(),
+							EnterpriseClassificationDataConcepts.GeographyXClassification,
+							system,
+							0,
+							classification, 
+							identityToken
+						)
+					);
+				}
+				
+				return current;
+			})
+			.chain(() -> classificationService.create(session, GeographyAdmin1AsciiCodes, system, GeographyClassifications))
+			.chain(() -> classificationService.create(session, GeographyAsciiName, system, GeographyAdmin1AsciiCodes))
+			
+			.chain(() -> {
+				logProgress("Geography Master", "Creating Geography Involved Parties");
+				// Create Identification Type
+				IInvolvedPartyService<?> involvedPartyService = get(IInvolvedPartyService.class);
+				return involvedPartyService.createIdentificationType(
+					session,
+					system, 
+					GeographyIPIdentificationTypes.ISP,
+					"An Internet Service Provider",
+					token
+				);
+			})
+			
+			.chain(idType -> {
+				logProgress("Geography Master", "Creating Planets");
+				// Create Planets and Continents by default
+				IGeographyService<?> service = get(IGeographyService.class);
+				return service.createPlanet(session, "Earth", null, system, identityToken);
+			})
+			
+			.chain(planet -> {
+				logProgress("Geography Master", "Creating Continents");
+				IGeographyService<?> service = get(IGeographyService.class);
+				
+				// Chain the creation of all continents
+				return service.createContinent(session, "Earth", new GeographyContinent().setContinentName("Africa")
+													 .setContinentCode("AF"), system, "6255146", identityToken)
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("Asia")
+													 .setContinentCode("AS"), system, "6255147", identityToken))
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("North America")
+													 .setContinentCode("NA"), system, "6255149", identityToken))
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("Europe")
+													 .setContinentCode("EU"), system, "6255148", identityToken))
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("Oceania")
+													 .setContinentCode("OC"), system, "6255151", identityToken))
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("South America")
+													 .setContinentCode("SA"), system, "6255150", identityToken))
+					.chain(() -> service.createContinent(session, "Earth", new GeographyContinent().setContinentName("Antarctica")
+													 .setContinentCode("AN"), system, "6255152", identityToken));
+			})
+			
+			.chain(() -> {
+				logProgress("Geography Master", "Creating Feature Classes");
+				return geonamesClassifications(session, enterprise);
+			})
+			.map(v -> true)
+			.onFailure().invoke(error -> log.error("Error during geography system installation: {}", error.getMessage(), error))
+			.onItem().invoke(() -> log.info("Geography system installation completed successfully"));
 	}
 
-	private Uni<Void> geonamesClassifications(IEnterprise<?,?> enterprise)
+	private Uni<Void> geonamesClassifications(Mutiny.Session session, IEnterprise<?,?> enterprise)
 	{
-		return Uni.createFrom().item(() -> {
-			log.info("Creating geonames classifications");
-			IClassificationService<?> classificationService = get(IClassificationService.class);
-			GeographySystem gs = get(GeographySystem.class);
-			ISystems<?,?> system = gs.getSystem(enterprise);
+		log.info("Creating geonames classifications");
+		IClassificationService<?> classificationService = get(IClassificationService.class);
+		GeographySystem gs = get(GeographySystem.class);
+		ISystems<?,?> system = gs.getSystem(enterprise);
 
-			// Country-related classifications
-			classificationService.create(GeographyAsciiName, system, Country);
-			classificationService.create(GeographyAdmin2Codes, system, City);
-
+		// Chain all classification creation operations sequentially
+		return classificationService.create(session, GeographyAsciiName, system, Country)
+			.chain(() -> classificationService.create(session, GeographyAdmin2Codes, system, City))
+			
 			// ISO and country info classifications
-			classificationService.create(CountryISO3166, system, Country);
-			classificationService.create(CountryISO3166_3, system, Country);
-			classificationService.create(CountryISO_Numeric, system, Country);
-			classificationService.create(CountryFips, system, Country);
-			classificationService.create(CountryCapital, system, Country);
-			classificationService.create(CountryAreaInSqKm, system, Country);
-			classificationService.create(CountryTld, system, Country);
-			classificationService.create(CurrencyCode, system, Country);
-			classificationService.create(CurrencyName, system, Country);
-			classificationService.create(CountryPhone, system, Country);
-			classificationService.create(CountryPostalCodeFormat, system, Country);
-			classificationService.create(CountryPostalCodeRegex, system, Country);
-			classificationService.create(CountryNeighbours, system, Country);
-			classificationService.create(CountryEquivalentFipsCode, system, Country);
-
+			.chain(() -> classificationService.create(session, CountryISO3166, system, Country))
+			.chain(() -> classificationService.create(session, CountryISO3166_3, system, Country))
+			.chain(() -> classificationService.create(session, CountryISO_Numeric, system, Country))
+			.chain(() -> classificationService.create(session, CountryFips, system, Country))
+			.chain(() -> classificationService.create(session, CountryCapital, system, Country))
+			.chain(() -> classificationService.create(session, CountryAreaInSqKm, system, Country))
+			.chain(() -> classificationService.create(session, CountryTld, system, Country))
+			.chain(() -> classificationService.create(session, CurrencyCode, system, Country))
+			.chain(() -> classificationService.create(session, CurrencyName, system, Country))
+			.chain(() -> classificationService.create(session, CountryPhone, system, Country))
+			.chain(() -> classificationService.create(session, CountryPostalCodeFormat, system, Country))
+			.chain(() -> classificationService.create(session, CountryPostalCodeRegex, system, Country))
+			.chain(() -> classificationService.create(session, CountryNeighbours, system, Country))
+			.chain(() -> classificationService.create(session, CountryEquivalentFipsCode, system, Country))
+			
 			// Timezone classifications
-			classificationService.create(TimeZoneOffsetJan2016, system, TimeZone);
-			classificationService.create(TimeZoneOffsetJuly2016, system, TimeZone);
-			classificationService.create(TimeZoneRawOffset, system, TimeZone);
-
+			.chain(() -> classificationService.create(session, TimeZoneOffsetJan2016, system, TimeZone))
+			.chain(() -> classificationService.create(session, TimeZoneOffsetJuly2016, system, TimeZone))
+			.chain(() -> classificationService.create(session, TimeZoneRawOffset, system, TimeZone))
+			
 			// Postal code classifications
-			classificationService.create(PostalNumber, system, PostalCode);
-			classificationService.create(PostalPlaceName, system, PostalCode);
-
-			log.info("Geonames classifications created successfully");
-			return null;
-		})
-		.onFailure().invoke(error -> log.error("Error creating geonames classifications: {}", error.getMessage(), error));
+			.chain(() -> classificationService.create(session, PostalNumber, system, PostalCode))
+			.chain(() -> classificationService.create(session, PostalPlaceName, system, PostalCode))
+			.onItem().invoke(() -> log.info("Geonames classifications created successfully"))
+			.onFailure().invoke(error -> log.error("Error creating geonames classifications: {}", error.getMessage(), error))
+			.replaceWithVoid();
 	}
 }
